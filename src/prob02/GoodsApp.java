22c249f66ba1bece972d36fc8193fb2be47702fc
BIT <BIT@DESKTOP-A70PMGU>
2018-02-23 11:10:29 +0900

    prac3 -3

--- src/prob02/GoodsApp.java
+++ src/prob02/GoodsApp.java
@@ -9,18 +9,16 @@ public class GoodsApp {
 		Scanner scanner = new Scanner(System.in);
 
 		Goods[] goods = new Goods[COUNT_GOODS];
-		//Goods goodsOri = null; 
 		System.out.println("상품을 담으세요.(3개)");
 		System.out.println("예) 상품명 가격 수량");
 		
 		for (int i = 0; i < COUNT_GOODS; i++) {
 			String temp = scanner.nextLine();
 			String[] tokens = temp.split(" ");
+			
 			Goods goodsOri = new Goods();
 			goodsOri.setItem(tokens[0]);
-			goods[i] = goodsOri;
 			goodsOri.setWon(Integer.parseInt(tokens[1]));
-			goods[i] = goodsOri;
 			goodsOri.setNumber(Integer.parseInt(tokens[2]));
 			goods[i] = goodsOri;
 		}
